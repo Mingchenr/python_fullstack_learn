1417a0bc6033c2bd452fec01078cb7a4428229ec
Mingchenr <134882229+Mingchenr@users.noreply.github.com>
2026-03-21 22:59:27 +0800

    Delete 02_django_core/django_learn/urls.py

--- 02_django_core/django_learn/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path("user/list/",views.user_list,name="user_list"),
-    path("user/info/<str:name>",views.user_info,name="user_info"),
-
-]
